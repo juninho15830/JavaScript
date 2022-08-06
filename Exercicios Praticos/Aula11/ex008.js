bce4224557e522dae448d4878c93b97cc0263780
Jair de Jesus Redigolo Junior <juninho15830@gmail.com>
2022-08-06 13:08:41 -0300

    Create ex008.js

---
+++ Exercicios Praticos/Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 101
+console.log(`A velocidade do seu carro é ${vel} quilometros por hora`)
+if (vel > 100){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}   //Condição simples
+
+console.log(`Dirija sempre usando o cinto de segurança`)
