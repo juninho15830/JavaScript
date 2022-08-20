d7430feb701ad9b8a5023e5282d4a25055044831
Jair de Jesus Redigolo Junior <juninho15830@gmail.com>
2022-08-19 21:51:17 -0300

    Update script.js

--- Exercicios Praticos/AulaEx03/scripts/script.js
+++ Exercicios Praticos/AulaEx03/scripts/script.js
@@ -2,11 +2,18 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
-    let res = getElementById('res')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        alert('t')
+        res.innerHTML = '<p>Contando: </p>' 
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} `
+        }
     }
 }
