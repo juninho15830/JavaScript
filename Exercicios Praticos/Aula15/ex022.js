679ce3bb73cafdc695a972d12d4fb7783acaa9f1
Jair de Jesus Redigolo Junior <juninho15830@gmail.com>
2022-08-25 21:30:34 -0300

    Create ex022.js

---
+++ Exercicios Praticos/Aula15/ex022.js
@@ -0,0 +1,25 @@
+let num = [5,8,2,9,3]
+num.push(1)
+num.sort()
+
+console.log(`Nosso vetor é o ${num}`)
+
+console.log(`O vetor tem ${num.length} posições.`)
+
+console.log(`O primeiro elemento do vetor é ${num[0]}`)
+
+/*
+for(let pos=0; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}Este comando mostra todos os valores e as respectivas chaves, mas o proximo faz o mesmo de maneira mais simples.
+*/
+
+for(let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}//for(..... in .....) Este se torna mais simples.
+
+let pos = num.indexOf(8)
+    if(pos == -1) {
+        console.log(`Elemento não encontrado`)
+    } else {
+        console.log(`A posição do elemento 8 é ${pos}`)}
